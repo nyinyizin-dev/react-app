b221ce4d6befd5d7047ad22274157aa479ca368b
nyinyizin-dev <nyinyizin450@gmail.com>
2025-08-21 19:10:51 +0900

    list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import "./App.css";
 
 function App() {
   let [name, setName] = useState("ko ko");
+  let [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "First post",
+    },
+    {
+      id: 2,
+      title: "Second post",
+    },
+    {
+      id: 3,
+      title: "Third post",
+    },
+  ]);
 
   let changeName = () => {
     setName("nyi nyi");
@@ -13,6 +27,13 @@ function App() {
     <div className="app">
       <h1>Hello : {name}</h1>
       <button onClick={changeName}>change name</button>
+
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
