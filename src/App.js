daa14ed29602034739a05d846e3658e902020e7e
nyinyizin-dev <nyinyizin450@gmail.com>
2025-08-21 18:37:42 +0900

    styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./App.css";
-import Image from "./assests/img-1.jpg";
 
 function App() {
   let name = "nyi nyi";
 
   return (
-    <div className="App">
-      <h1>hello : {name}</h1>
-      {/* 1- public folder way */}
-
-      {/* realtive path */}
-      {/* <img src="/img-1.jpg" alt="" /> */}
-      {/* absolute path */}
-      {/* <img src="http://localhost:3000/img-1.jpg" alt="" /> */}
-
-      {/* 2- import way */}
-      <img src={Image} alt="" />
+    <div className="app">
+      <h1>Hello : {name}</h1>
     </div>
   );
 }
