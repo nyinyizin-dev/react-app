2841a52c36d92c3ccd87627c52f5d3785e6822f0
nyinyizin-dev <nyinyizin450@gmail.com>
2025-08-21 19:22:25 +0900

    count with pre state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  let [name, setName] = useState("ko ko");
-  let [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "First post",
-    },
-    {
-      id: 2,
-      title: "Second post",
-    },
-    {
-      id: 3,
-      title: "Third post",
-    },
-  ]);
+export default function App() {
+  let [count, setCount] = useState(0);
 
-  let changeName = () => {
-    setName("nyi nyi");
-    console.log(name);
+  let increment = () => {
+    setCount((pre)=>pre+1);
+    setCount((pre)=>pre+1);
+    setCount((pre)=>pre+1);
+    setCount((pre)=>pre+1);
   };
-
   return (
-    <div className="app">
-      <h1>Hello : {name}</h1>
-      <button onClick={changeName}>change name</button>
-
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+    <div>
+      <h1>Counter</h1>
+      <p>count - {count}</p>
+      <button onClick={increment}>increment</button>
     </div>
   );
 }
-
-export default App;
