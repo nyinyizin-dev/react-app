1f3e027ec7c3289a8dea77c62774b05b22bc77b7
nyinyizin-dev <nyinyizin450@gmail.com>
2025-08-23 08:48:00 +0900

    dynamic style and module.css

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,18 +2,12 @@ import "./index.css";
 import ReactDOM from "react-dom";
 
 export default function Modal({ children, danger = false }) {
+  let className = danger ? "border-red" : "border-blue";
+
   return ReactDOM.createPortal(
     <div className="modal-component">
       <div className="modal-backdrop">
-        <div
-          className="modal"
-          style={{
-            border: "4px solid",
-            borderColor: danger ? "red" : "orange",
-          }}
-        >
-          {children}
-        </div>
+        <div className={`modal ${className}`}>{children}</div>
       </div>
     </div>,
     document.getElementById("modal")
