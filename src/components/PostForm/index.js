95dd37b5d7a0d441a8aa34f5395369e5135ce6d0
nyinyizin-dev <nyinyizin450@gmail.com>
2025-08-23 16:38:36 +0900

    two way data binding

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -4,15 +4,20 @@ import "./index.css";
 export default function PostForm() {
   let [title, setTitle] = useState("");
 
+ let resetFrom = () => {
+  setTitle('');
+  console.log("updated successfully");
+ }
+
   return (
     <form className="post-form">
       <h1>creat post</h1>
       <div className="form-control">
         <label>Title</label>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
       </div>
       <p>{title}</p>
-
+      <button type="button" onClick={resetFrom}>reset form</button>
       <div className="form-control">
         <button>Post now</button>
       </div>
